66458291a180563a93f6ddd1ca1dd3914f8da024
Vaith <vaithwee@yeah.net>
2020-02-04 18:03:41 +0800

    Added home future component

---
+++ src/views/home/child/HomeFuture.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-future">
+        <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeFuture"
+    }
+</script>
+
+<style scoped>
+.home-future img {
+    width: 100%;
+}
+</style>

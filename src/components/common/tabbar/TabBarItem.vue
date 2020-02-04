a0cb7c3c0e10def2ff94695a785c6537450fad4b
Vaith <vaithwee@yeah.net>
2020-02-04 10:56:03 +0800

    Added router and components

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="tab-bar-item" @click="itemClick">
+        <slot v-if="!isActive" name="item-icon"></slot>
+        <slot v-else name="item-icon-active"></slot>
+        <div :class="{active: isActive}"><slot name="item-text"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem",
+        props: {
+            path : {
+                type: String
+            },
+        },
+        data() {
+            return {
+                // isActive: true,
+            }
+        },
+        computed: {
+            isActive() {
+                return this.$route.path.indexOf(this.path) !== -1;
+            }
+        },
+        methods: {
+            itemClick() {
+                console.log("item click");
+                console.log(this.path);
+                this.$router.replace(this.path);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .tab-bar-item {
+        flex: 1;
+        text-align: center;
+        font-size: 14px;
+        height: 49px;
+    }
+    .tab-bar-item img {
+        width: 24px;
+        height: 24px;
+        margin-top: 2px;
+    }
+    .active {
+        color: red;
+    }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav-bar class="home-nav">
+            <div slot="center">购物街</div>
+        </nav-bar>
+        <h2>home</h2>
+    </div>
+</template>
+
+<script>
+    import NavBar from "@/components/common/nav/NavBar";
+    export default {
+        name: "Home",
+        components: {
+            NavBar
+        }
+    }
+</script>
+
+<style scoped>
+.home-nav {
+    background-color: var(--color-tint);
+    line-height: 44px;
+    color: white;
+}
+</style>

b087740606a04675d753ac44dcf7dc62f5cfbf34
Vaith <vaithwee@yeah.net>
2020-02-04 17:02:56 +0800

    Added home banner component

---
+++ src/network/request.js
@@ -0,0 +1,21 @@
+import Axios from "axios";
+export function request(config) {
+    const instance = Axios.create({
+        baseURL: 'http://123.207.32.32:8000/',
+        timeout: 60000,
+    });
+
+    instance.interceptors.request.use(config => {
+        return config;
+    }, error => {
+        console.log(error);
+    });
+
+    instance.interceptors.response.use(res => {
+        return res.data;
+    }, error => {
+        console.log(error);
+    });
+
+    return instance(config);
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,35 @@
         <nav-bar class="home-nav">
             <div slot="center">购物街</div>
         </nav-bar>
-        <h2>home</h2>
+        <home-swiper :banners="banner"></home-swiper>
     </div>
 </template>
 
 <script>
     import NavBar from "@/components/common/nav/NavBar";
+    import {getHomeMultiData} from "@/network/home";
+    import HomeSwiper from "@/views/home/child/HomeSwiper";
+
     export default {
         name: "Home",
+        data() {
+            return {
+                result: null,
+                banner: null,
+                recommend: null,
+            }
+        },
         components: {
-            NavBar
+            NavBar,
+            HomeSwiper,
+        },
+        created() {
+            getHomeMultiData().then(res => {
+                console.log(res);
+                this.result = res;
+                this.banner = res.data.banner.list;
+                this.recommend = res.data.recommend.list;
+            })
         }
     }
 </script>
